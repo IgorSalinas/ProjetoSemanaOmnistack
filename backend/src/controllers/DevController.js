937e00129b99f180fac3ea1bdbb1d3d82a4e8d8a
Igor <igor_salinas@outlook.com>
2020-01-26 13:44:54 -0300

    Real Time implementado

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 const Dev = require('../models/Dev');
 const parseStringAsArray = require('../utils/parseStringAsArray');
+const { findConnections, sendMessage } = require('../websocket');
 
 module.exports = {
 
@@ -35,6 +36,16 @@ module.exports = {
                 techs: techsArray,
                 location,
             })
+
+            // Filtrar as conexões que estão há no máximo 10k de distãncia 
+            // e que o novo dev tenha pelo menus uma das tecnologias filtradas
+
+            const sendSocketMessageTo = findConnections(
+                { latitude, longitude },
+                techsArray,
+            )
+
+            sendMessage(sendSocketMessageTo, 'new-dev', dev);
         }
 
         return response.json(dev);
